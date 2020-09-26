0b54da40a9b7185304bbcb1cc9b98dd7c298d9b1
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-09-26 11:13:06 +0900

    Create index and create actions in users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+  def index
+    user = User.find_by(uid: params[:uid])
+    render json: user
+  end
+
+  def create
+    user = User.new(user.params)
+
+    if user.save
+      render json: user, status: :created
+    else
+      render json: user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :uid)
+  end
 end
