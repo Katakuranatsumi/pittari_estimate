1a87a01d03f54734e88c5377c3facf15de6d2891
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-09-30 22:26:53 +0900

    Make Signup.vue

---
+++ pittari-estimate-front/src/components/Signup.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="signup">
+    <h2>Sign up</h2>
+    <input type="email" placeholder="Email" v-model="email">
+    <input type="password" placeholder="Password" v-model="password">
+    <button @click="signUp">Register</button>
+  </div>
+</template>
+
+<script>
+import firebase from '@/plugins/firebase'
+export default {
+  name: 'Signup',
+  data() {
+    return {
+      email: '',
+      password: '',
+      user: null,
+    }
+  },
+  methods: {
+    signUp: function() {
+      firebase.auth().createUser(this.email, this.password)
+        .then(user => {
+          alert("Create account!")
+          let newUser = {
+            uid: user.user.uid,
+            email: this.email
+          }
+        })
+        .catch(error => {
+          alert(error.message)
+        })
+    }
+  }
+}
+</script>
