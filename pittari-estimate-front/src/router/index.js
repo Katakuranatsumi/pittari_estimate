66f485e29a6740c1ed0716dcaf62e5261af4f24d
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-10-01 21:37:23 +0900

    Make routes.js

--- pittari-estimate-front/src/router/index.js
+++ pittari-estimate-front/src/router/index.js
@@ -1,28 +1,8 @@
 import Router from 'vue-router'
-import SignIn from '@/views/SignIn'
-import SignUp from '@/views/SignUp'
-import User from '@/views/users/User'
+// import User from '@/views/users/User'
 import Vue from 'vue'
+import routes from './routes'
 
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
-  routes: [
-    {
-     path: '/signup',
-     name: 'signup',
-     component: SignUp
-    },
-    {
-     path: '/signin',
-     name: 'signin',
-     component: SignIn
-    },
-    {
-     path: '/users',
-     name: 'User',
-     component: User
-    },
-  ]
-})
+export default new Router({ routes })
---
+++ pittari-estimate-front/src/router/routes.js
@@ -0,0 +1,7 @@
+// import SignIn from '@/views/SignIn'
+import SignUp from '@/components/Signup.vue'
+
+export default [{
+  path: '/signup',
+  component: SignUp
+}]
