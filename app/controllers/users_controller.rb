c04422d4aa2ae4f2117fcb4b51d7ed6f324db524
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-10-06 21:55:22 +0900

    Implemented to be able to register User in Rails db using axios

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user.params)
+    user = User.new(user_params)
 
     if user.save
       render json: user, status: :created
--- pittari-estimate-front/src/components/templates/Signup.vue
+++ pittari-estimate-front/src/components/templates/Signup.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="signup">
     <h2>Sign up</h2>
+    <input type="name" placeholder="Name" v-model="name">
     <input type="email" placeholder="Email" v-model="email">
     <input type="password" placeholder="Password" v-model="password">
     <button @click="signUp">Register</button>
@@ -9,10 +10,13 @@
 
 <script>
 import firebase from '@/plugins/firebase'
+import axios from '@/plugins/axios'
+
 export default {
   name: 'Signup',
   data() {
     return {
+      name: '',
       email: '',
       password: '',
       user: null,
@@ -24,9 +28,11 @@ export default {
         .then(user => {
           alert("Create account!")
           let newUser = {
+            name: this.name,
             uid: user.user.uid,
             email: this.email
           }
+          axios.post('/users', newUser)
         })
         .catch(error => {
           alert(error.message)
