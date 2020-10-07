5e072abe2089d2782b45d5740063e9fafe93f485
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-10-07 22:04:46 +0900

    Change schema.rb due to add Task model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_014628) do
+ActiveRecord::Schema.define(version: 2020_10_07_130150) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "detail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
