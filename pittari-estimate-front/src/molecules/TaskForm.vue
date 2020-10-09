bd72f37c7b58578cb7ec0c84e62b08beae8e3f13
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-10-09 21:48:52 +0900

    Add TaskForm.vue

---
+++ pittari-estimate-front/src/molecules/TaskForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <form>
+    <div class="form-item">
+      <label for="title">タイトル</label>
+      <input
+        id="title"
+        placeholder="例：ログイン機能の追加">
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'TaskForm'
+}
+</script>
