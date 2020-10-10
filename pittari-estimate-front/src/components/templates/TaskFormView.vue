e8b6191ee300515acb901d8f5428fef0096da374
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-10-10 11:19:27 +0900

    Add TaskFormView.vue

---
+++ pittari-estimate-front/src/components/templates/TaskFormView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="taskform">
+    <TaskForm></TaskForm>
+  </div>
+</template>
+
+<script>
+import TaskForm from '@/molecules/TaskForm.vue'
+
+export default {
+  name: 'TaskFormVue',
+  components: {
+    TaskForm
+  },
+}
+</script>
