8bf2b7af37e56905fe8b40fa87606e71a7bad2e0
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-10-11 17:30:44 +0900

    Add Button.vue

---
+++ pittari-estimate-front/src/components/atoms/Button.vue
@@ -0,0 +1,26 @@
+<template>
+  <button
+    :class="classes"
+    type="button"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+
+  props: {
+    type: {
+      type: String,
+      default: 'button'
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+}
+</script>
