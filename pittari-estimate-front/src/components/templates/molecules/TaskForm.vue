c438b676fbd2a5c24a666629f6e793402d6c9af4
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-10-11 17:32:50 +0900

    Move molecules folder to components folder

---
+++ pittari-estimate-front/src/components/templates/molecules/TaskForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form>
+    <div class="form-item">
+      <label for="title">タイトル</label>
+      <input
+        id="title"
+        type="text"
+        required
+        placeholder="例：ログイン機能の追加">
+    </div>
+    <div>
+      <label for="detail">詳細</label>
+      <input
+        id="detail"
+        type="text"
+        placeholder="例：Railsでログイン機能を実装する"
+      >
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'TaskForm',
+
+  data () {
+    return {
+      title: '',
+      detail: ''
+    }
+  },
+}
+</script>
