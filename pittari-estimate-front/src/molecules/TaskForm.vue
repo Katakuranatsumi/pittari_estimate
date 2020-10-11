58f74ec6bd65abf7182b1cee5af185bf4c5369bd
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-10-11 16:56:05 +0900

    Add detail part of TaskForm

--- pittari-estimate-front/src/molecules/TaskForm.vue
+++ pittari-estimate-front/src/molecules/TaskForm.vue
@@ -4,13 +4,30 @@
       <label for="title">タイトル</label>
       <input
         id="title"
+        type="text"
+        required
         placeholder="例：ログイン機能の追加">
+    </div>
+    <div>
+      <label for="detail">詳細</label>
+      <input
+        id="detail"
+        type="text"
+        placeholder="例：Railsでログイン機能を実装する"
+      >
     </div>
   </form>
 </template>
 
 <script>
 export default {
-  name: 'TaskForm'
+  name: 'TaskForm',
+
+  data () {
+    return {
+      title: '',
+      detail: ''
+    }
+  },
 }
 </script>
