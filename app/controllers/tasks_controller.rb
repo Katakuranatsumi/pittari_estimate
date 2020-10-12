4199e3951cfa342fc9e9d07401104e0708db1a3e
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-10-13 00:02:40 +0900

    Implement create action in tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,5 +6,18 @@ class TasksController < ApplicationController
   end
 
   def create
+    task = Task.new(task_params)
+
+    if task.save
+      render json: task, status: :created
+    else
+      render json: task.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :detail)
   end
 end
