127c9d8c588ee5c3bcf9e9b6edf2c7dfac07cbf6
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-10-15 21:58:54 +0900

    Add validation to task quote input form

--- pittari-estimate-front/src/components/molecules/TaskForm.vue
+++ pittari-estimate-front/src/components/molecules/TaskForm.vue
@@ -1,5 +1,6 @@
 <template>
   <form>
+    <h2>タスク見積もり登録フォーム</h2>
     <div class="form-item">
       <label for="title">タイトル</label>
       <input
@@ -8,6 +9,9 @@
         v-model="title"
         required
         placeholder="例：ログイン機能の追加">
+      <ul class="validation-errors">
+        <p v-if="!validation.title.required">タイトルが入力されていません。</p>
+      </ul>
     </div>
     <div>
       <label for="detail">詳細</label>
@@ -20,6 +24,7 @@
     </div>
     <div class="form-actions">
       <Button
+        :disabled="disableTaskCreateAciton"
         @click="handleClick"
       >
         タスク見積登録
@@ -30,6 +35,7 @@
 
 <script>
 import axios from '@/plugins/axios'
+const required = val => !!val.trim()
 
 export default {
   name: 'TaskForm',
@@ -37,7 +43,35 @@ export default {
   data () {
     return {
       title: '',
-      detail: ''
+      detail: '',
+      progress: false
+    }
+  },
+
+  computed: {
+    validation () {
+      // titleとdetailのバリデーション
+      return {
+        title: { required: required(this.title) }
+      }
+    },
+
+    valid () {
+      const validation = this.validation
+      const fields = Object.keys(validation)
+      let valid = true
+      for (let i =0; i < fields.length; i++) {
+        const field = fields[i]
+        valid = Object.keys(validation[field])
+          .every(key => validation[fields][key])
+        if (!valid) { break }
+      }
+      return valid
+    },
+
+    disableTaskCreateAciton () {
+      // validを使用してログイン処理の可否を判別
+      return !this.valid || this.progress
     }
   },
 
@@ -61,3 +95,11 @@ export default {
   }
 }
 </script>
+
+<style>
+
+.validation-errors {
+  height: 32px;
+  color:orangered;
+}
+</style>
