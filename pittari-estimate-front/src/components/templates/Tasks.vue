a52bed67485d425d88f1efb8ae5242889d7e97a9
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-10-16 23:07:12 +0900

    Implement so that title and detail are displayed in a list

--- pittari-estimate-front/src/components/templates/Tasks.vue
+++ pittari-estimate-front/src/components/templates/Tasks.vue
@@ -1,13 +1,34 @@
 <template>
   <div class="tasks">
     <h2>タスク一覧</h2>
+    <li v-for="task in tasks" v-bind:key="task">
+      タイトル：{{ task["title"] }}
+      <br>
+      詳細：{{ task["detail"] }}
+    </li>
   </div>
 </template>
 
 <script>
+import axios from '@/plugins/axios'
+
 export default {
   name: 'Tasks',
+
   data() {
+    return {
+      tasks: null
+    }
   },
+
+  mounted() {
+    axios.get('/tasks')
+      .then((response) => {
+        this.tasks = response.data
+      })
+      .catch((err) => {
+        this.error = err.message
+      })
+  }
 }
 </script>
