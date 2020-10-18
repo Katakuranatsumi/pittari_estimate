642dfdec9d03b03807dd0814a8c03f7f89882793
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-10-18 20:45:19 +0900

    Implement to extract Task data from Rails

--- pittari-estimate-front/src/components/templates/TaskDetailView.vue
+++ pittari-estimate-front/src/components/templates/TaskDetailView.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="task-detail">
     <h2>タスク見積もり詳細</h2>
+    <div>タイトル： {{ task["title"] }}</div>
   </div>
 </template>
+
+<script>
+import axios from '@/plugins/axios'
+
+export default {
+  name: 'TaskDetailView',
+  data: function () {
+    return {
+      task : null
+    }
+  },
+
+  mounted() {
+    axios.get('/tasks/' + this.$route.params.taskId)
+      .then((response) => {
+        this.task = response.data
+      })
+      .catch((err) => {
+        this.error = err.message
+      })
+  }
+}
+</script>
