9d6e1494c9bdde9f08a9ca58f78f43f0d28ea960
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-10-24 11:31:48 +0900

    Add fontawesome to main.js

--- pittari-estimate-front/src/main.js
+++ pittari-estimate-front/src/main.js
@@ -1,7 +1,15 @@
-import App from './App.vue'
-import Vue from 'vue/dist/vue.esm'
-import router from './router/index'
-import Router from 'vue-router'
+import App from './App.vue';
+import Vue from 'vue/dist/vue.esm';
+import router from './router/index';
+import Router from 'vue-router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, far, fab);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(Router)
 Vue.config.productionTip = false
