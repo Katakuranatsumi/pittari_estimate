998e221be072810a7ce1e27f55aefb90124fb3fc
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-11-10 22:40:53 +0900

    Move edit form to EditTaskFormView.vue

--- pittari-estimate-front/src/components/templates/EditTaskFormView.vue
+++ pittari-estimate-front/src/components/templates/EditTaskFormView.vue
@@ -1,35 +1,50 @@
 <template>
-  <div class="task-detail">
-    <div>
-      <h2>タスク見積もり詳細</h2>
-      <div>タイトル： {{ task["title"] }}</div>
-      <div>作成日時：{{ createdTime }}</div>
-      <div>詳細：{{ task["detail"] }}</div>
-      <router-link :to="{ path: '/tasks/' + this.$route.params.taskId + '/edit' }">
-        <font-awesome-icon icon="edit"/>
-      </router-link>
-      <router-link :to="{ path: '/tasks' }">タスク一覧画面に戻る</router-link>
+  <form>
+    <h2>タスク見積もり編集</h2>
+    <div class="form-item">
+      <label for="title">タイトル：</label>
+      <input
+        id="title"
+        type="text"
+        v-model="title">
     </div>
-  </div>
+    <div>
+      <label for="detail">詳細：</label>
+      <input
+        id="detail"
+        type="text"
+        v-model="detail">
+    </div>
+    <div class="form-actions">
+      <vue-button
+        @click="update"
+      >
+        更新
+      </vue-button>
+      <vue-button>
+        キャンセル
+      </vue-button>
+    </div>
+  </form>
 </template>
 
 <script>
 import axios from '@/plugins/axios'
+import VueButton from '@/components/atoms/Button'
 
 export default {
-  name: 'TaskDetailView',
+  name: 'EditTaskFormView',
+
+  components: {
+    VueButton
+  },
 
   data: function() {
     return {
       task : '',
+      title: '',
+      detail: ''
     }
-  },
-
-  computed: {
-    createdTime: function() {
-      const createdTime = new Date(this.task["created_at"]).toLocaleString();
-      return createdTime
-    },
   },
 
   methods: {
@@ -53,6 +68,8 @@ export default {
     axios.get('/tasks/' + this.$route.params.taskId)
       .then((response) => {
         this.task = response.data
+        this.title = this.task["title"]
+        this.detail = this.task["detail"]
       })
       .catch((err) => {
         this.error = err.message
