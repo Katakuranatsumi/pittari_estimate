00689193afa609b6cd829e3ddba94e2221b8143f
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-11-10 22:57:49 +0900

    Add title validation to edit form

--- pittari-estimate-front/src/components/templates/EditTaskFormView.vue
+++ pittari-estimate-front/src/components/templates/EditTaskFormView.vue
@@ -15,6 +15,9 @@
         type="text"
         v-model="detail">
     </div>
+    <ul class="validation-errors">
+      <p v-if="!validation.title.required">タイトルが入力されていません。</p>
+    </ul>
     <div class="form-actions">
       <vue-button
         @click="update"
@@ -33,6 +36,7 @@
 <script>
 import axios from '@/plugins/axios'
 import VueButton from '@/components/atoms/Button'
+const required = val => !!val.trim()
 
 export default {
   name: 'EditTaskFormView',
@@ -47,6 +51,33 @@ export default {
       title: '',
       detail: ''
     }
+  },
+
+  computed: {
+    validation: function() {
+      // titleとdetailのバリデーション
+      return {
+        title: { required: required(this.title) }
+      }
+    },
+
+    valid: function() {
+      const validation = this.validation
+      const fields = Object.keys(validation)
+      let valid = true
+      for (let i =0; i < fields.length; i++) {
+        const field = fields[i]
+        valid = Object.keys(validation[field])
+          .every(key => validation[fields][key])
+        if (!valid) { break }
+      }
+      return valid
+    },
+
+    disableTaskCreateAciton: function() {
+      // validを使用してログイン処理の可否を判別
+      return !this.valid || this.progress
+    },
   },
 
   methods: {
