cc93b18c597dfdd5499543bd18d8ea1bc5f076a7
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-11-22 22:36:09 +0900

    Add setTasks to mutations

--- pittari-estimate-front/src/store/mutations.js
+++ pittari-estimate-front/src/store/mutations.js
@@ -1,4 +1,10 @@
+import Vue from 'vue/dist/vue.esm';
+
 export default {
+  setTasks (state, { item }) {
+    Vue.set(state.items, item)
+  },
+
   getTasks (state, data) {
     state.tasks = data;
   }
