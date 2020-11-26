23c5eaf041fff7ffa46f95666bdc7ebe453fe461
KatakuraNatsumi <n.katakura@everyleaf.com>
2020-11-26 22:28:33 +0900

    Fix CSS in TaskDetail.vue

--- pittari-estimate-front/src/components/templates/TaskDetailView.vue
+++ pittari-estimate-front/src/components/templates/TaskDetailView.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="task-detail">
     <div>
-      <h2>タスク見積もり詳細</h2>
-      <div>タイトル： {{ task["title"] }}</div>
-      <div>作成日時：{{ createdTime }}</div>
-      <div>詳細：{{ task["detail"] }}</div>
-      <router-link :to="{ path: '/tasks/' + this.$route.params.taskId + '/edit' }">
+      <h2 class="title block">タスク見積もり詳細</h2>
+      <div class="content is-medium">タイトル： {{ task["title"] }}</div>
+      <div class="content is-medium">作成日時：{{ createdTime }}</div>
+      <div class="content is-medium">詳細：{{ task["detail"] }}</div>
+      <router-link
+        class="button mr-1"
+        :to="{ path: '/tasks/' + this.$route.params.taskId + '/edit' }">
         編集<font-awesome-icon icon="edit"/>
       </router-link>
-      <router-link :to="{ path: '/tasks' }">タスク一覧画面に戻る</router-link>
+      <router-link
+        class="button"
+        :to="{ path: '/tasks' }">タスク一覧画面に戻る
+      </router-link>
     </div>
   </div>
 </template>
